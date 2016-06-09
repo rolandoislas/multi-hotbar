9816001c03b01296d185d24a0c05783ccc1b1ba0
Rolando Islas <admin@rolandoislas.com>
2016-06-09 15:59:59 -0700

    Set server to use max hotbars for compatibility

--- src/main/java/com/rolandoislas/multihotbar/Config.java
+++ src/main/java/com/rolandoislas/multihotbar/Config.java
@@ -1,5 +1,7 @@
 package com.rolandoislas.multihotbar;
 
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.relauncher.Side;
 import net.minecraftforge.common.config.Configuration;
 
 import java.io.File;
@@ -12,14 +14,24 @@ public class Config {
     private static Configuration config;
 
     public static void load(File suggestedConfigurationFile) {
+        // Check if server and set hotbars to max
+        boolean server = FMLCommonHandler.instance().getSide() == Side.SERVER;
+        if (server) {
+            numberOfHotbars = 4;
+            return;
+        }
+        // Handle client config
         config = new Configuration(suggestedConfigurationFile);
         config.load();
         numberOfHotbars = config.getInt("Number of Hotbars", Configuration.CATEGORY_GENERAL, 2, 1, 2,
-                "Defines the amount fo hotbars that should be displayed");
+                "Defines the amount of hotbars that should be displayed");
         config.save();
     }
 
     public static void reload() {
-        load(config.getConfigFile());
+        if (config != null)
+            load(config.getConfigFile());
+        else
+            load(null);
     }
 }
